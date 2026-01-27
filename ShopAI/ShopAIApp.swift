1e1e6489ba570f03bf4609ba6efa336053528722
Hakan Alsancak <hakanalsancak03@gmail.com>
2026-01-27 12:17:53 +0000

    Initial Commit

---
+++ ShopAI/ShopAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopAIApp.swift
+//  ShopAI
+//
+//  Created by Hakan Alsancak on 27/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct ShopAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
